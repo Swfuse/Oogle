0f8ff1c60599030c25e799f9b3f95d8c0eb63510
Swfuse <swfusez@gmail.com>
2018-08-14 20:24:31 +0300

    Create base page with form and logo image

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome to my Doodle</title>
+</head>
+<body>
+    <div class="mainSection">
+        <div class="logoContainer">
+            <img src="/img/logo.png" alt="Logo myDoodle">
+        </div>
+        <div class="searchContainer">
+            <form action="search.php">
+                <input type="text" class="searchBox" name="term" placeholder="Enter a search term...">
+                <input type="submit" class="searchButton" value="Search">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
